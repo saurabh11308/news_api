8513564fa40fb52dcc50e1b3716f3ac2901d6df2
Saurabh <saurabh11308@yahoo.com>
2026-07-16 17:42:02 +0530

    Added code

--- news_api.py
+++ news_api.py
@@ -6,7 +6,16 @@ url = "https://newsdata.io/api/1/latest? \
   &country=in\
   &language=hi,en"
 
+#Made Request to the api here
 request = requests.get(url)
-content = request.text
+#Got the dictionary of items here
+content = request.json()
 
-print(content)
+result = content['results']
+
+#Printed the title,image,description,published date here
+for r in result:
+    print(r['title'])
+    print(r['description'])
+    print(r['image_url'])
+    print(r['pubDate'])
