b97f50e259e8e6289e573fbf3b453c0920a99fcc
Saurabh <saurabh11308@yahoo.com>
2026-07-16 19:32:02 +0530

    FIne improvements

--- news_api.py
+++ news_api.py
@@ -1,23 +1,32 @@
+from pydoc_data.topics import topics
+
 import requests
 from send_email import send_email
 
-url = "https://newsdata.io/api/1/latest? \
-  apikey=pub_0f46a3e35d664a1d84f729d600cba41e \
-  &q=IDBI BANK \
-  &country=in\
-  &language=hi,en"
+topic = "IDBI BANK"
+key = "pub_0f46a3e35d664a1d84f729d600cba41e"
+language = "hi,en"
+
+url = "https://newsdata.io/api/1/latest?" \
+  f"apikey={key}" \
+  f"&q={topic}" \
+  "&country=in"\
+  f"&language={language}"
 
 #Made Request to the api here
 request = requests.get(url)
 #Got the dictionary of items here
 content = request.json()
 
-result = content['results']
+#We want 5 items from the list
+result = content['results'][:5]
 
 news_string = ""
 
 #Printed the title,image,description,published date here
 for index,r in enumerate(result):
+    #Adding subject to the email
+    news_string += "Subject: Today's news" + '\n'
     news_string += f"News {index+1}" + '\n'
     news_string += r['title'] + '\n'
     news_string += r['description'] + '\n'
